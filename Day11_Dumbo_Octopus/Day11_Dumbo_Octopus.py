20e5c73d623fbcb35f3e9f099fe7e307a04fdc8b
mv.toropov <mv.toropov@tensor.ru>
2021-12-27 13:18:20 +0300

    Day11_Dumbo_Octopus

--- Day11_Dumbo_Octopus/Day11_Dumbo_Octopus.py
+++ Day11_Dumbo_Octopus/Day11_Dumbo_Octopus.py
@@ -7,60 +7,74 @@ class Point:
 ADJACENT = {
     "up": [-1, 0],
     "up_right": [-1, 1],
-    "right": [0, 1],
-    "down_right": [1, 1],
+    "up_left": [-1, -1],
     "down": [1, 0],
+    "down_right": [1, 1],
     "down_left": [1, -1],
+    "right": [0, 1],
     "left": [0, -1],
-    "up_left": [-1, -1]
 }
 
 
 def is_on_field(puzzle, point, changes):
     if (0 <= point.y + changes[0] < len(puzzle)
-        and 0 <= point.x + changes[1] < len(puzzle[point.y + changes[0]])):
+            and 0 <= point.x + changes[1] < len(puzzle[point.y + changes[0]])):
         return True
     return False
 
 
 def do_flash(puzzle, point):
+    puzzle[point.y][point.x] = 0
     for key in ADJACENT:
         if is_on_field(puzzle, point, ADJACENT[key]):
-            new_point = Point(point.x + ADJACENT[key][1], point.y + ADJACENT[key][0])
-            puzzle[new_point.y][new_point.x] += 1
-    puzzle[point.y][point.x] = 0
+            new_point = Point(point.x + ADJACENT[key][1],
+                              point.y + ADJACENT[key][0])
+            if puzzle[new_point.y][new_point.x] > 0:
+                puzzle[new_point.y][new_point.x] += 1
 
 
-def define_flash(puzzle):
+def make_step(puzzle):
+    to_flash = define_flash(puzzle, 9)
+    flashes = 0
+    for y in range(len(puzzle)):
+        for x in range(len(puzzle[y])):
+            puzzle[y][x] += 1
+    while to_flash:
+        for point in to_flash:
+            do_flash(puzzle, point)
+            flashes += 1
+        to_flash = define_flash(puzzle, 10)
+    return flashes
+
+
+def define_flash(puzzle, level):
     to_flash = []
     for y in range(len(puzzle)):
         for x in range(len(puzzle[y])):
-            if puzzle[y][x] >= 9:
+            if puzzle[y][x] >= level:
                 to_flash.append(Point(x, y))
     return to_flash
 
 
-def make_step(puzzle):
-    to_flash = define_flash(puzzle)
-    for y in range(len(puzzle)):
-        for x in range(len(puzzle[y])):
-            if puzzle[y][x] < 9:
-                puzzle[y][x] += 1
-    while to_flash:
-        for point in to_flash:
-            do_flash(puzzle, point)
-        to_flash = define_flash(puzzle)
-
-
 def main():
     puzzle = []
-    steps = 100
-    with open("Day11_Dumbo_Octopus/Day11_Dumbo_Octopus2.txt") as f:
+    # steps = 10
+    # steps = 100
+    steps = 1000
+    flashes = 0
+    sim_flash = 0
+    step_sim_flash = -1
+    with open("Day11_Dumbo_Octopus/Day11_Dumbo_Octopus.txt") as f:
         for line in f:
             puzzle.append(list(map(int, list(line.replace('\n', '')))))
     for step in range(steps):
-        make_step(puzzle)
-    print(puzzle)
+        sim_flash = make_step(puzzle)
+        if sim_flash == len(puzzle) * len(puzzle[0]) and step_sim_flash == -1:
+            step_sim_flash = step + 1
+        flashes += sim_flash
+    print(flashes)
+    print(step_sim_flash)
+
 
 if __name__ == "__main__":
     main()
